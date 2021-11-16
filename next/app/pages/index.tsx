11b319ac561109b954b04ff93804951d784450c2
marnysan111 <o-masato@outlook.jp>
2021-11-16 06:11:47 +0000

    tailwindCSSの導入

--- next/app/pages/index.tsx
+++ next/app/pages/index.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
     <div>
         <Link href="/link">
-          <a>移動するよぉ</a>
+          <h1 className="text-red-600">移動するよぉ</h1>
         </Link>
     </div>
   )
