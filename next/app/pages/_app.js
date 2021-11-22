414ecb62e7dfa5b00778c087e7eacbff02a9c46d
marnysan111 <o-masato@outlook.jp>
2021-11-22 07:24:57 +0000

    classNameにしよう

--- next/app/pages/_app.js
+++ next/app/pages/_app.js
@@ -5,8 +5,8 @@ import '../image/sangikousen.png'
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div class="header" className="grid-cols-12">
-        <div class="fontColor">
+      <div className="grid-cols-12 header">
+        <div className="fontColor">
           <div className="text-5xl">AIスマート工学コース</div>
         </div> 
       </div>
