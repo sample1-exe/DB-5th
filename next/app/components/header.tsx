43f3e06b709da7e2dc456371ef4087bd56bf3480
Lafolia13 <violgdae@gmail.com>
2021-11-28 17:13:58 +0900

    明らかに誤用してる箇所を修正、リンクを追加

--- next/app/components/header.tsx
+++ next/app/components/header.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header(props) {
     return (
@@ -7,18 +8,38 @@ export default function Header(props) {
     <Head>
         <title>{props.title}</title>
     </Head>
-    <header className="header grid xl:grid-cols-2 lg:grid-cols-2 items-center">
+    <header className="header grid grid-cols-2 items-center px-2">
         <div className="text-5xl justify-left fontColor hidden lg:inline-block">
             AIスマート工学コース
         </div>
-        <div
-        className="xl:col-end-3 lg:con-end-3 flex lg:justify-end md:justify-center">
-        <Image 
-            src="/sangikousen-removebg.png"
-            width="360"
-            height="80"
-            className=""
-        />
+        <div className="col-end-3 col-span-2 lg:col-span-1 flex justify-center lg:justify-end">
+            <Image 
+                src="/sangikousen-removebg.png"
+                width="360"
+                height="80"
+                className=""
+            />
+        </div>
+
+        <div className="col-start-1 col-end-3 lg:col-end-2 flex justify-between text-center fontColor border-b-2 border-black">
+            <Link href="/">
+                コース案内
+            </Link>
+            <Link href="/">
+                コース活動
+            </Link>
+            <Link href="/">
+                授業
+            </Link>
+            <Link href="/">
+                教員
+            </Link>
+            <Link href="/">
+                キャリア
+            </Link>
+            <Link href="/">
+                連携協定
+            </Link>
         </div>
     </header>
     </>
