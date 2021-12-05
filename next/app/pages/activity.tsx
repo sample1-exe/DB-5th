5ddea9595135c8fb50cbefd66d3cd9f0676ff0a0
Lafolia13 <violgdae@gmail.com>
2021-12-05 23:05:53 +0900

    コース活動

--- next/app/pages/activity.tsx
+++ next/app/pages/activity.tsx
@@ -15,63 +15,11 @@ export default function Activity() {
             コース活動
           </div>
 
-          <ActivityTable />
+          現在、活動はありません。
         </div>
       
         <Footer />
       </div>
     </>
   )
-}
-
-interface ActivityData {
-  date: string,
-  title: string,
-  image: string,
-  detail: string,
-}
-
-const ActivityTable = () => {
-  /*
-      活動一覧を DB から取得するやつを誰かが書く
-      {
-          ActivityData[]
-      }
-  */
-
-  // モック
-  const newsDatas: Array<ActivityData> = [];
-  for (let i = 0; i < 2; i++) {
-      const data: ActivityData = {
-          date: "1111-11-11",
-          title: "New job: fix Mr. Gluck’s hazy TV, PDQ!" + i.toString(10),
-          image: '/mock.jpg',
-          detail: 'いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえあさきゆめみしゑひもせす'
-      };
-
-      newsDatas.push(data);
-  }
-
-  return (<>
-      <table className="table-auto">
-          <tbody>
-              {newsDatas.map((data) => 
-                  <tr key={data.title}>
-                      <td>
-                          <img src={data.image} className="h-64" />
-                      </td>
-                      <td>
-                        <div className="border-b-2 border-black text-2xl font-bold">
-                          <span className="pr-8">{data.title}</span>
-                          <span>{data.date}</span>
-                        </div>
-                        <div>
-                          {data.detail}
-                        </div>
-                      </td>
-                  </tr>
-              )}
-          </tbody>
-      </table>
-  </>);
 }
