4ee10835b259a723160d627f2fa31a98ad12d034
Lafolia13 <violgdae@gmail.com>
2021-12-05 22:04:38 +0900

    トップページ

--- next/app/components/index/event.tsx
+++ next/app/components/index/event.tsx
@@ -1,15 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Event() {
     return (
     <>
         <div className="grid grid-cols-1 py-8">
-            <Link href="/">
+            <Link href="https://www.metro-cit.ac.jp/kousen.html">
                 <img
                     src="/mock.jpg"
-                    width="100%"
+                    width="80%"
+                    className="mx-auto"
                 />
             </Link>
         </div>
--- next/app/components/index/news.tsx
+++ next/app/components/index/news.tsx
@@ -1,64 +1,43 @@
-import Link from 'next/link';
-
-export default function News() {
+export default function News({ newsJson }) {
     return (
     <>
         <div className="lg:w-9/12 mx-auto px-4 grid grid-cols-1">
             <div className="text-center">
                 ニュース
             </div>
-            <NewsTable />
-            {/* <div className="text-center py-4">
-                <button className="pastNews">
-                    <Link href="/newslist">
-                        過去のニュースはこちらから
-                    </Link>
-                </button>
-            </div> */}
+            <NewsTable newsJson={newsJson}/>
         </div>
     </>
     )
 }
 
 interface NewsData {
+    id: number,
     date: string,
     headline: string,
-    url: string
 }
 
-const NewsTable = () => {
-    /*
-        ニュース一覧を DB から取得するやつを誰かが書く
-        {
-            NewsData[]
-        }
-    */
-
-    // モック
+const NewsTable = (newsJson) => {
     const newsDatas: Array<NewsData> = [];
-    for (let i = 0; i < 10; i++) {
+    for (const newsData of newsJson.newsJson) {
         const data: NewsData = {
-            date: "1111-11-11",
-            headline: "New job: fix Mr. Gluck’s hazy TV, PDQ!",
-            url: i.toString(10)
-        };
-        data.url = "/newsilst/" + data.url;
-
-        newsDatas.push(data);
+            id: newsData.ID,
+            date: newsData.UpdatedAt,
+            headline: newsData.News_data
+        }
+        newsDatas.push(data)
     }
 
     return (<>
         <table className="border-b-2 border-t-2">
             <tbody>
                 {newsDatas.map((data) => 
-                    <tr key={data.url}>
+                    <tr key={data.id}>
                         <td className="font-bold">
                             {data.date}
                         </td>
                         <td>
-                            {/* <Link href={data.url}> */}
-                                {data.headline}
-                            {/* </Link> */}
+                            {data.headline}
                         </td>
                     </tr>
                 )}
