eb3fcef76722b72583159aba5732167b5a9bf7db
Lafolia13 <violgdae@gmail.com>
2021-12-05 23:04:21 +0900

    教員

--- next/app/pages/teacher.tsx
+++ next/app/pages/teacher.tsx
@@ -1,7 +1,14 @@
+import { GetServerSideProps } from 'next'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-export default function Activity() {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const teacherJson = await fetch("http://db5th_go:8080/select/teacher")
+      .then(res => res.json())
+  return { props: { teacherJson } }
+}
+
+export default function Activity(teacherJson) {
   const title: string = "教員 | AIスマート工学コース";
   return ( 
     <>
@@ -15,7 +22,7 @@ export default function Activity() {
             教員
           </div>
 
-          <TeacherTable />
+          <TeacherTable teacherJson={teacherJson.teacherJson} />
         </div>
       
         <Footer />
@@ -30,48 +37,30 @@ interface TeacherData {
   faceURL: string
 }
 
-const TeacherTable = () => {
-  /*
-      活動一覧を DB から取得するやつを誰かが書く
-      {
-          ActivityData[]
-      }
-  */
-
-  // モック
+const TeacherTable = (teacherJson) => {
+  console.log(teacherJson.teacherJson)
   const teacherDatas: Array<TeacherData> = [];
-  for (let i = 0; i < 10; i++) {
+  for (const teacherData of teacherJson.teacherJson) {
       const data: TeacherData = {
-        teacherName: "天上人" + i.toString(10),
-        subject: "帝王学",
-        faceURL: "./mock.jpg"
+        teacherName: teacherData.Teacher_name,
+        subject: "",
+        faceURL: "./who.png"
       };
 
       teacherDatas.push(data);
   }
 
   return (<>
-      <table className="table-auto">
-          <tbody>
-              {teacherDatas.map((data) => 
-                  <tr key={data.teacherName}>
-                      <td>
-                        <div className="border-b-2 border-black text-2xl font-bold">
-                          <span className="pr-8">{data.teacherName}</span>
-                        </div>
-                        <div>
-                          担当教科
-                        </div>
-                        <div>
-                          {data.subject}
-                        </div>
-                      </td>
-                      <td>
-                          <img src={data.faceURL} className="h-64" />
-                      </td>
-                  </tr>
-              )}
-          </tbody>
-      </table>
+        {teacherDatas.map((data) =>
+          <div className="grid grid-cols-2 pb-4">
+            <div className="col-span-1">
+              <div className="border-b-2 border-black text-2xl font-bold">
+                {data.teacherName}
+              </div>
+              担当教科
+            </div>
+            <img src="./who.png" className="h-32 p-4"/>
+          </div>
+        )}
   </>);
 }
